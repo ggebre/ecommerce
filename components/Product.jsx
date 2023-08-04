8921d84373f87840c2c9e57d6a7dd4e30108bd70
Getu Gebre <getu.gebre.gg@gmail.com>
2023-08-04 12:17:18 -0500

    update product component link

--- components/Product.jsx
+++ components/Product.jsx
@@ -4,21 +4,20 @@ import Link from 'next/link';
 import { urlFor } from '@/lib/client';
 
 const Product = ({product: {image, name, slug, price}}) => {
+    console.log(slug.current)
     return (
-        <div> 
-            <Link href={`/product/${slug.current}`} />
             <div className='product-card' >
-                <img 
-                    // src={urlFor(image && image[0])}
-                    width={250}
-                    height={250}
-                    className='product-image' 
-                />
-                <p className='product-name'>{name}</p>
-                <p className='product-price'>{price}</p>
-            </div>
-        
-        </div>
+                <Link href={`/product/${slug.current}`}>
+                    <img 
+                        // src={urlFor(image && image[0])}
+                        width={250}
+                        height={250}
+                        className='product-image' 
+                    />
+                    <p className='product-name'>{name}</p>
+                    <p className='product-price'>{price}</p>
+                </Link>
+            </div>     
     )
 }
 
