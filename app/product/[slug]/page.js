211162271f4c874746357241ef11bdd39be5fee6
Getu Gebre <getu.gebre.gg@gmail.com>
2023-08-19 16:05:52 -0500

    add class to image list

--- app/product/[slug]/page.js
+++ app/product/[slug]/page.js
@@ -22,7 +22,7 @@ async function ProductDetail ({params}) {
                         <img 
                             key={i}
                             src={urlFor(item.image[0])}
-                            className=''
+                            className='small-image'
                             onMouseEnter=""
                         />
                     ))}
